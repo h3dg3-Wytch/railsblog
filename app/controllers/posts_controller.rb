b5b9ec5b004c3239785db50eb7aea35aa48a0aa3
Rex <rexpeters3n@gmail.com>
2017-01-20 16:12:17 -0500

    Add comment model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,12 +14,33 @@ class PostsController < ApplicationController
 		if @post.save
 			redirect_to @post
 		else
-			redier new
+			render 'new'
 		end
 	end
 
 	def show
 		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+		
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to root_path
 	end
 
 	private 
